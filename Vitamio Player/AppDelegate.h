0195bfaffc823e3e202a58b6545a650d916e0d64
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-01-02 10:30:07 +0530

    Initial Commit

---
+++ Vitamio Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vitamio Player
+//
+//  Created by Tecksky Techonologies on 1/2/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vitamio Player/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vitamio Player
+//
+//  Created by Tecksky Techonologies on 1/2/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
